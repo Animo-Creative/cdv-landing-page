03345e4aa1fd5a3c83e6643daf80b77bffa08778
Igor Ribeiro <igor@animocreative.com.br>
2022-11-28 17:31:23 -0300

    feat: created container

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,4 +1,5 @@
 import FooterComponent from "../../components/Footer";
+import FormComponent from "../../components/Form";
 import HeaderComponent from "../../components/Header";
 
 
@@ -6,6 +7,20 @@ function Home() {
   return (
     <div>
       <HeaderComponent/>
+      <FormComponent/>
+      <div width="100vw" height="500px">
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </div>
       <FooterComponent/>
     </div>
   );
